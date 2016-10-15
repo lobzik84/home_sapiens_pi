edf934d597fff5eaea7e7facc8643551c43efd5e
Nikolas <alfanick@yandex.ru>
2016-10-15 21:01:54 +0300

    Update GraphicsModule.java

--- src/java/org/lobzik/home_sapiens/pi/modules/GraphicsModule.java
+++ src/java/org/lobzik/home_sapiens/pi/modules/GraphicsModule.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.util.Date;
 import org.apache.log4j.Logger;
@@ -35,7 +36,10 @@ public class GraphicsModule implements Module {
     public final String MODULE_NAME = this.getClass().getSimpleName();
     private static GraphicsModule instance = null;
     private static Connection conn = null;
-    private Process process = null;
+    private static Process process = null;
+    private static PrintWriter pw = null;
+    private static boolean run = true;
+
     private static Logger log = null;
     private static final String PREFIX = "/usr/bin/sudo";
     private static final String COMMAND = "/usr/bin/fbi";
@@ -61,6 +65,48 @@ public class GraphicsModule implements Module {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+        try {
+            conn = DBTools.openConnection(BoxCommonData.dataSourceName);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }      
+
+        try {
+            File file = new File(AppData.getGraphicsWorkDir().getAbsolutePath() + File.separator + "screen.jpg");
+            startProcess(file);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+                
+        
+    }
+    
+    public static void startProcess(File file) {
+        try {
+            while (run) {
+
+                String[] env = {"aaa=bbb", "ccc=ddd"};
+                String[] args = {PREFIX, COMMAND, AppData.getGraphicsWorkDir().getAbsolutePath() + File.separator + file};
+                File workdir = AppData.getGraphicsWorkDir();
+                Runtime runtime = Runtime.getRuntime();
+                //log.debug("Drawing " + txt);
+                process = runtime.exec(args, env, workdir);
+
+                GraphicsModule.StreamGobbler errorGobbler = new GraphicsModule.StreamGobbler(process.getErrorStream());
+                GraphicsModule.StreamGobbler outputGobbler = new GraphicsModule.StreamGobbler(process.getInputStream());
+                errorGobbler.start();
+                outputGobbler.start();
+
+                pw = new PrintWriter( process.getOutputStream() );
+
+                process.waitFor();
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
     }
 
     @Override
@@ -68,6 +114,10 @@ public class GraphicsModule implements Module {
         if (e.type == Event.Type.USER_ACTION && e.name.equals("show_image")) {
             draw((String)e.data.get("image_file"));
         }
+        if (e.type == Event.Type.TIMER_EVENT && e.name.equals("internal_sensors_poll")) {
+            draw("");
+        }
+        
     }
     
         private void draw(String txt) {
@@ -101,26 +151,15 @@ public class GraphicsModule implements Module {
                         
             File file = new File(AppData.getGraphicsWorkDir().getAbsolutePath() + File.separator + "newimage.png");
             ImageIO.write(img, "png", file);
-    
-            String[] env = {"aaa=bbb", "ccc=ddd"};
 
-            String[] args = {PREFIX, COMMAND, AppData.getGraphicsWorkDir().getAbsolutePath() + File.separator + file};
-            File workdir = AppData.getGraphicsWorkDir();
-            Runtime runtime = Runtime.getRuntime();
-            log.debug("Drawing " + txt);
-            process = runtime.exec(args, env, workdir);
-            
-            GraphicsModule.StreamGobbler errorGobbler = new GraphicsModule.StreamGobbler(process.getErrorStream());
-            GraphicsModule.StreamGobbler outputGobbler = new GraphicsModule.StreamGobbler(process.getInputStream());
-            errorGobbler.start();
-            outputGobbler.start();
-            //process.waitFor();
-            int exitValue = process.exitValue();
-            //log.debug(StreamGobbler.getAllOutput());
-            GraphicsModule.StreamGobbler.clearOutput();
-            if (exitValue != 0) {
-                log.error("Error executing, exit status: " + exitValue);
+            if(!process.isAlive()){
+                startProcess(file);
             }
+            else {
+                pw.print(" ");
+            }
+    
+
         } catch (Exception e) {
             log.error("Error " + e.getMessage());
         }
@@ -128,7 +167,15 @@ public class GraphicsModule implements Module {
     }
 
     public static void finish() {
+        run = false;
         DBTools.closeConnection(conn);
+        process.destroy();
+        int exitValue = process.exitValue();
+            //log.debug(StreamGobbler.getAllOutput());
+            GraphicsModule.StreamGobbler.clearOutput();
+            if (exitValue != 0) {
+                log.error("Error executing, exit status: " + exitValue);
+            }
     }
     
     
