f65d017928b8af0364474c359323aa4013ac5290
Nikolas <alfanick@yandex.ru>
2016-11-09 11:27:57 +0300

    Create WebNotificationsModule.java

---
+++ src/java/org/lobzik/home_sapiens/pi/modules/WebNotificationsModule.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lobzik.home_sapiens.pi.modules;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.lobzik.home_sapiens.pi.AppData;
+import org.lobzik.home_sapiens.pi.ConnJDBCAppender;
+import org.lobzik.home_sapiens.pi.event.Event;
+import org.lobzik.home_sapiens.pi.event.EventManager;
+import org.lobzik.tools.StreamGobbler;
+
+/**
+ *
+ * @author lobzik
+ */
+public class WebNotificationsModule implements Module {
+
+    public final String MODULE_NAME = this.getClass().getSimpleName();
+    private static WebNotificationsModule instance = null;
+    private Process process = null;
+    private static Logger log = null;
+    private static final String PREFIX = "/usr/bin/sudo";
+    private static final String SHUTDOWN_COMMAND = "halt";
+    private static final String SHUTDOWN_SUFFIX = "-p";
+
+    private WebNotificationsModule() { //singleton
+    }
+
+    public static WebNotificationsModule getInstance() {
+        if (instance == null) {
+            instance = new WebNotificationsModule(); //lazy init
+            log = Logger.getLogger(instance.MODULE_NAME);
+            Appender appender = ConnJDBCAppender.getAppenderInstance(AppData.dataSource, instance.MODULE_NAME);
+            log.addAppender(appender);
+        }
+        return instance;
+    }
+
+    @Override
+    public String getModuleName() {
+        return MODULE_NAME;
+    }
+
+    @Override
+    public void start() {
+        try {
+            EventManager.subscribeForEventType(this, Event.Type.SYSTEM_EVENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void webNotify() {
+        try {
+            String[] env = {"aaa=bbb", "ccc=ddd"};
+
+            String[] args = {PREFIX, SHUTDOWN_COMMAND, SHUTDOWN_SUFFIX};
+            File workdir = AppData.getSoundWorkDir();
+            Runtime runtime = Runtime.getRuntime();
+            log.info("Shutting down system");
+            process = runtime.exec(args, env, workdir);
+
+            StringBuilder output = new StringBuilder();
+            StreamGobbler errorGobbler = new StreamGobbler(process.getErrorStream(), output);
+            StreamGobbler outputGobbler = new StreamGobbler(process.getInputStream(), output);
+            errorGobbler.start();
+            outputGobbler.start();
+            process.waitFor();
+            int exitValue = process.exitValue();
+            //log.debug(StreamGobbler.getAllOutput());
+
+            if (exitValue != 0) {
+                log.error("Error executing, exit status: " + exitValue);
+            }
+        } catch (Exception e) {
+            log.error("Error " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void handleEvent(Event e) {
+        if (e.type == Event.Type.SYSTEM_EVENT && e.name.equals("webNotify")) {
+            webNotify();
+        }
+    }
+
+    public static void finish() {
+
+    }
+
+}
